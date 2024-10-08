c164edb613cfab28b86adb3bc25266c0918b714a
Jacky <cunkuaiz0850@gmail.com>
2024-10-07 20:04:26 -0400

    Finalized Halloween game with sound effects and animations

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(SpookyHalloweenGame());
@@ -21,10 +22,12 @@ class SpookyHomePage extends StatefulWidget {
   _SpookyHomePageState createState() => _SpookyHomePageState();
 }
 
-class _SpookyHomePageState extends State<SpookyHomePage> with SingleTickerProviderStateMixin {
+class _SpookyHomePageState extends State<SpookyHomePage>
+    with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation _animation;
   Random random = Random();
+  final AudioPlayer _player = AudioPlayer();
 
   @override
   void initState() {
@@ -33,12 +36,31 @@ class _SpookyHomePageState extends State<SpookyHomePage> with SingleTickerProvid
       duration: const Duration(seconds: 5),
       vsync: this,
     )..repeat(reverse: true);
+
     _animation = Tween(begin: -50.0, end: 50.0).animate(_controller);
+    playBackgroundMusic();
+  }
+
+  void playBackgroundMusic() async {
+    await _player.setSource(AssetSource('lib/assets/sounds/spooky_music.wav')); // Play local file
+    _player.setReleaseMode(ReleaseMode.loop); // Loop the background music
+    await _player.resume(); // Start playing
+  }
+
+  void playJumpScareSound() async {
+    await _player.setSource(AssetSource('lib/assets/sounds/jump_scare.wav')); // Play local file
+    await _player.resume(); // Start playing the jump scare sound
+  }
+
+  void playSuccessSound() async {
+    await _player.setSource(AssetSource('lib/assets/sounds/success_sound.mp3')); // Play local file
+    await _player.resume(); // Start playing the success sound
   }
 
   @override
   void dispose() {
     _controller.dispose();
+    _player.dispose();
     super.dispose();
   }
 
@@ -48,24 +70,31 @@ class _SpookyHomePageState extends State<SpookyHomePage> with SingleTickerProvid
       appBar: AppBar(title: Text('Find the Correct Halloween Item!')),
       body: Stack(
         children: [
-          // Background image
           Positioned.fill(
             child: Image.asset(
               'lib/assets/background1.png',
               fit: BoxFit.cover,
             ),
           ),
-          // Ghost image
           Positioned(
             left: random.nextDouble() * MediaQuery.of(context).size.width,
             top: _animation.value,
-            child: Image.asset('lib/assets/ghost.png', width: 80, height: 80),
+            child: GestureDetector(
+              onTap: () {
+                playJumpScareSound();
+              },
+              child: Image.asset('lib/assets/ghost.png', width: 80, height: 80),
+            ),
           ),
-          // Pumpkin icon
           Positioned(
             right: random.nextDouble() * MediaQuery.of(context).size.width,
             bottom: _animation.value,
-            child: Icon(Icons.emoji_emotions, size: 80, color: Colors.orange),
+            child: GestureDetector(
+              onTap: () {
+                playSuccessSound();
+              },
+              child: Icon(Icons.emoji_emotions, size: 80, color: Colors.orange),
+            ),
           ),
         ],
       ),
